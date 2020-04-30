37070bfc22fe91057426accde4b9d0452e561981
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-04-29 23:04:58 -0400

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
@@ -9,9 +7,7 @@ class RouteServiceProvider extends ServiceProvider
 {
     /**
      * This namespace is applied to your controller routes.
-     *
      * In addition, it is set as the URL generator's root namespace.
-     *
      * @var string
      */
     protected $namespace = 'App\Http\Controllers';
