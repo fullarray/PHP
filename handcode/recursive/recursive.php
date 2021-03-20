bec432b220e18900b47da8c1849e3842a1eb2394
Jonathan <sifreed77@gmail.com>
2021-03-19 21:23:24 -0400

    Update recursive.php

--- handcode/recursive/recursive.php
+++ handcode/recursive/recursive.php
@@ -1,9 +1,7 @@
 <?php
 	function printFibonacciRec($num){
 		if($num==0){
-			
 		}else if($num == 1){
-			
 		}else{
 			//Recursive call
 			return(printFibonacciRec($num-1) + printFibonacciRec($num-2));
