b3f840045cb059ce46c97012fcf99221e74a09d5
Jonathan <sifreed77@gmail.com>
2021-04-17 22:36:08 -0400

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 |-------------------------------------------------------
 | Create The Applications
 |-------------------------------------------------------
+
+
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
 | the IoC container for the system binding all of the various parts.
