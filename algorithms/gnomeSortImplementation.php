eb4bb9dc30e72b0bf24afc4b5e67c3ac444db9bb
Jonathan <sifreed77@gmail.com>
2021-04-21 22:18:27 -0400

    Update gnomeSortImplementation.php

--- algorithms/gnomeSortImplementation.php
+++ algorithms/gnomeSortImplementation.php
@@ -1,9 +1,13 @@
 public static function gnomeSort(array $array){
 	$i = 1;
 	while($i < count($array)){
+				  
 		if($array[$i] >= $array[$i - 1]){
+			
 			$i++;
+		
 		}else{
+		
 			$tmp = $array[$i];
 			$array[$i] = $array[$i - 1];
 			$array[$i - 1] = $tmp;
