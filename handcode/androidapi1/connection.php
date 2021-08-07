f629a6a74a88d56f6ca8c4abd9e7b2900bb67c98
Jonathan <sifreed77@gmail.com>
2021-08-06 22:31:45 -0400

    Update connection.php

--- handcode/androidapi1/connection.php
+++ handcode/androidapi1/connection.php
@@ -4,7 +4,7 @@
 		private $connect;
 		function __construct(){
 			$this->connect = mysqli_connect(hostname, user, password, db_name)
-			or die("Could not connect to db.");
+			or die("Could not connect to database.");
 		}
 		function getConnection(){
 			return $this->connect;
