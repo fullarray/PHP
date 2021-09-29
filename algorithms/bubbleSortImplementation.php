392ca2af29712bb2904bcfed7667fbd765ed17d0
Jonathan <sifreed77@gmail.com>
2021-09-28 23:21:22 -0400

    Update bubbleSortImplementation.php

--- algorithms/bubbleSortImplementation.php
+++ algorithms/bubbleSortImplementation.php
@@ -2,7 +2,6 @@ function bubbleSort($array){
 	if(!$length = count($array)){
 		return $array;
 	}
-
         //Loops through every element
 	for($outer=0; $outer < $length; $outer++){
 		for($inner=0; $inner < $length; $inner++){
